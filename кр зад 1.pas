bf08ea8f8112159e28e505cdc7727008ba9910a9
Ulyanka2901 <144227029+Ulyanka2901@users.noreply.github.com>
2023-10-27 10:23:03 +0300

    Add files via upload

---
+++ кр зад 1.pas
@@ -0,0 +1,22 @@
+﻿program zad1;
+var x,flag,y:real;
+begin
+  write ('введите значение x=');
+  read(x);
+  if (x<-10) then 
+    y:=cos(x)*sqr(x);
+  if (-10<=x) and (x<-4) then
+    y:=power(x,(0.1*x/31))*power(exp(x),x/59);
+  if (-4<=x) and (x<5) then
+    y:= ln(x);
+  if (5<=x) then
+    y:= x*sqr(x)*x-cos(x);
+  if y=y then
+    flag:=1
+  else
+    flag:=0;
+  if flag = 1 then
+    write('x=' , x , ' ' , 'y=' , y:0:2)
+  else
+    write('x=' , x , ' ' , 'Такого нет');
+end.
---
+++ кр зад 2.pas
@@ -0,0 +1,25 @@
+﻿Program zad2;
+var x,y,flag:real;
+begin
+  x:= -12;
+  while x<=7 do
+  begin
+if (x<-10) then 
+    y:=cos(x)*sqr(x);
+  if (-10<=x) and (x<-4) then
+    y:=power(x,(0.1*x/31))*power(exp(x),x/59);
+  if (-4<=x) and (x<5) then
+    y:= ln(x);
+  if (5<=x) then
+    y:= x*sqr(x)*x-cos(x);
+  if y=y then
+    flag:=1
+  else
+    flag:=0;
+  if flag = 1 then
+    writeln('x=' , x:0:2 , '  ' , 'y=' , y:0:4)
+  else
+    writeln('x=' , x:0:2 , '  ' , 'y=корней нет');
+  x:= x + 0.1
+  end;
+  end.
